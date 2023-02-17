41a770e1c2098e27b5ae9830a5a45a361478b209
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2023-02-17 23:18:11 +0000

    Fixed a bug in capture.js

--- src/capture.js
+++ src/capture.js
@@ -2,5 +2,5 @@ import { addHotkey } from "./util/hotkeys.js";
 import { tapKeys, Key } from "./util/instantKeyboard.js";
 
 addHotkey(["NUMPAD DIVIDE", "C"], _ => {
-	tapKeys(Key.LeftSuper, Key.LeftAlt, Key.G);
+	tapKeys([Key.LeftSuper, Key.LeftAlt, Key.G]);
 });
--- src/util/instantKeyboard.js
+++ src/util/instantKeyboard.js
@@ -4,9 +4,10 @@ export { Key } from "@nut-tree/nut-js";
 export const keyboard = new KeyboardClass(providerRegistry);
 keyboard.config.autoDelayMs = 0;
 
-export async function tapKeys(keys) {
+export async function tapKeys(keys, duration = 500) {
 	if (! Array.isArray(keys)) keys = [keys];
 
 	await keyboard.pressKey(...keys);
+	await new Promise(resolve => setTimeout(resolve, duration));
 	await keyboard.releaseKey(...keys);
 };
